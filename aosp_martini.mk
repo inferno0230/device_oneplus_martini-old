767bcca961ee353a4637628fb35b74f73360ffef
Aditya Choudhury <adityachoudhury0230@gmail.com>
2022-12-22 09:40:32 +0530

    martini: Initial Pixel Experience bringup

--- aosp_martini.mk
+++ aosp_martini.mk
@@ -12,12 +12,11 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 $(call inherit-product, device/oneplus/martini/device.mk)
 
 # Inherit some common Lineage stuff.
-$(call inherit-product, vendor/lineage/config/common_full_phone.mk)
+$(call inherit-product, vendor/aosp/config/common_full_phone.mk)
 
 
 TARGET_BOOT_ANIMATION_RES := 1080
-
-PRODUCT_NAME := lineage_martini
+PRODUCT_NAME := aosp_martini
 PRODUCT_DEVICE := martini
 PRODUCT_MANUFACTURER := OnePlus
 PRODUCT_BRAND := OnePlus
